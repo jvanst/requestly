8c6219693febddbfb9a7f88dbdfef5b56e142c2a
James Vansteenkiste <james@jamesvan.ca>
2019-05-21 19:56:39 -0400

    update theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,14 +4,35 @@ import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loade
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 
-Vue.use(Vuetify, {
-  icons: {
-    iconfont: 'md' // default
-  }
-})
+Vue.use(Vuetify)
 
 export default new Vuetify({
   theme: {
-    dark: false
+    dark: false,
+    themes: {
+      light: {
+        primary: '#009688',
+        secondary: '#FF9800',
+        accent: '#D7EEFA',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107',
+        typhography: '#212121'
+      },
+      dark: {
+        primary: '#009688',
+        secondary: '#FF9800',
+        accent: '#D7EEFA',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107',
+        typhography: '#212121'
+      }
+    }
+  },
+  icons: {
+    iconfont: 'mdi'
   }
 })
